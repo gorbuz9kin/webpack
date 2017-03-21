ff2900183ea7e7d7730dbc6b0c689b6163143700
Denis Gorobzeev <denis.gorobzeev@gmail.com>
2017-03-21 16:34:25 +0200

    add webpack-server

--- frontend/home.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import welcome from './welcome.js';
-
-welcome("home");
-
-exports.welcome = welcome;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 let ExtractTextPlugin = require("extract-text-webpack-plugin");
+//let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    context: __dirname + '/frontend',
+    context: __dirname + '/src',
     entry: {
-        home: "./home",
-        about: "./about"
+        main: [
+        	'webpack-dev-server/client?http://localhost:8080',
+        	'./js/main'
+        ],
+        styles: './styles/main.styl'
     },
     output: {
-        path: __dirname + '/public/js',
-        filename: "[name].js",
-        library: "[name]"
+        path: __dirname + '/build/',
+        filename: "./js/[name].js",
+	    publicPath: "/"
     },
 
-    watch: NODE_ENV == 'development',
-
-    watchOptions: {
-        aggregateTimeout: 100
-    },
-
-   devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : null,
+   devtool: "cheap-inline-module-source-map",
 
    plugins: [
        new webpack.NoErrorsPlugin(),
@@ -31,7 +30,29 @@ module.exports = {
            name: "common",
            chunks: ['home', 'about']
        }),
-       new ExtractTextPlugin('./css/styles.css')
+       new ExtractTextPlugin('./css/main.css', {allChunks: true}),
+	   /*new HtmlWebpackPlugin({
+		        template: './index.html'
+           }),*/
+	   new webpack.HotModuleReplacementPlugin(),
+	   new CopyWebpackPlugin([
+		   {
+			   from: './img',
+			   to: 'img/'
+		   },
+		   {
+			   context: './',
+			   from: '**/*.html',
+			   to: './'
+		   },
+		   {
+			   context: './',
+			   from: '**.pug',
+			   to: './'
+		   },
+	   ], {
+		   copyUnmodified: true
+	   })
    ],
 
    resolve: {
@@ -53,16 +74,20 @@ module.exports = {
                plugins: ['transform-runtime']
            },
            {
+	           test: /\.html$/,
+	           loader: 'file?name=[path][name].[ext]'
+           },
+           {
                test: /\.pug$/,
-               loader: 'pug'
+               loader: 'pug-loader'
            },
            {
                test: /\.css$/,
-               loader: ExtractTextPlugin.extract('style', 'css!autoprefixer?browser=last 2 version')
+               loader: ExtractTextPlugin.extract('style', 'css!autoprefixer?browsers=last 2 version')
            },
             {
                test: /\.styl$/,
-               loader: ExtractTextPlugin.extract('style' ,'css!autoprefixer?browser=last 2 version!stylus?resolve url')
+               loader: ExtractTextPlugin.extract('css!autoprefixer?browsers=last 2 version!stylus?resolve url')
            },
            {
                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
@@ -70,17 +95,12 @@ module.exports = {
            }
        ]
    },
-
+    
+   devServer: {
+       inline: true,
+	   historyApiFallback: true,
+	   outputPath: "./build"
+   }
+   
+   
 };
-
-if (NODE_ENV == 'production') {
-    module.exports.plugins.push (
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                drop_console: true,
-                unsafe: true
-            }
-        })
-    );
-}
